8adb8da1863c860e1038bbf67fd7c434546019bc
JacquelineMai <jacquelinemai@umail.ucsb.edu>
2020-08-17 22:55:09 -0700

    jm - added support for fromStates & example in recorder.yaml init steps

--- src/main/scala/com/research/nomad/markii/dataflow/custom/FromConfig.scala
+++ src/main/scala/com/research/nomad/markii/dataflow/custom/FromConfig.scala
@@ -15,13 +15,16 @@ import scala.jdk.CollectionConverters._
 import cats.syntax.either._
 import io.circe._
 import io.circe.generic.auto._
+import io.circe.generic.semiauto._
 import io.circe.yaml
 
+case class Transition(fromStates: Option[Set[String]], toState: String)
 case class APISemanticConfig(baseClassName: String, states: List[String], initialState: String,
-                             transferMap: Map[String, String])
+                             transferMap: Map[String, Transition])
 
 class FromConfig(configPath: String) extends CustomObjectStateTransformer[AbsValSet[String]] {
   type D = AbsValSet[String]
+  implicit val transitionDecoder : Decoder[Transition] = deriveDecoder[Transition];
 
   private val json = yaml.parser.parse(new FileReader(configPath))
 
@@ -31,6 +34,7 @@ class FromConfig(configPath: String) extends CustomObjectStateTransformer[AbsVal
     .valueOr(throw _)
 
   override def initInstance(sootClass: SootClass): Option[D] = {
+    print("JSON: ", json);
     if (sootClass.getName == api.baseClassName) {
       Some(AbsValSet(Set(api.initialState)))
     } else {
@@ -45,17 +49,32 @@ class FromConfig(configPath: String) extends CustomObjectStateTransformer[AbsVal
   private val transitionSites = mutable.Map[Stmt, mutable.Set[(Option[Ref], String, String)]]()
 
   def getTransitions(m: SootMethod): Set[(Option[Ref], String, String)] = {
+      
+  print("HERE!!", api.transferMap);
     m.getActiveBody.getUnits.asScala.flatMap(u => transitionSites.getOrElse(u.asInstanceOf[Stmt], Set())).toSet
+ 
   }
 
   override def updatedInstance(prevVals: D, instanceInvokeExpr: InstanceInvokeExpr, callSite: Stmt, ref: Option[Ref]): D = {
-    api.transferMap.get(instanceInvokeExpr.getMethod.getSignature) match {
-      case Some(nextState) =>
+  print("HERE");
+   api.transferMap.get(instanceInvokeExpr.getMethod.getSignature) match {
+      case Some(transition) =>
+        print("HERE: ", transition);
+        // var inFromSet : Boolean = false;
         for (prevVal <- prevVals.vals) {
-          transitionSites.getOrElseUpdate(callSite, mutable.Set()).add((ref, prevVal, nextState))
+          if( transition.fromStates.isEmpty || transition.fromStates.contains(prevVal)){
+            // inFromSet = true;
+            transitionSites.getOrElseUpdate(callSite, mutable.Set()).add((ref, prevVal, transition.toState))
+          }
         }
-        AbsValSet(Set(nextState))
-      case None => prevVals
+        // if(inFromSet)
+         AbsValSet(Set(transition.toState))
+        // else{
+        //   None
+        // }
+      case None =>
+        print("HERE: NONE" );
+        prevVals
     }
   }
 }
