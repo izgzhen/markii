337fc662d1048cf3fee5d616c887753e017a20e9
Luxi Wang <louis99@cs.washington.edu>
2020-11-22 15:57:57 -0800

    cannot find buttons positive/negative
    Patch
    
    cannot find buttons positive/negative
    
    Patch

--- src/main/scala/com/research/nomad/markii/instrument/AllInstrument.scala
+++ src/main/scala/com/research/nomad/markii/instrument/AllInstrument.scala
@@ -4,7 +4,8 @@
 
 package com.research.nomad.markii.instrument
 
-import com.research.nomad.markii.{CallGraphManager, Constants}
+import com.research.nomad.markii.dataflow.Ref
+import com.research.nomad.markii.{AppInfo, CallGraphManager, Constants, DynamicCFG}
 import soot.{Local, RefType, SootClass, SootMethod}
 import soot.jimple.{InstanceInvokeExpr, Jimple, Stmt}
 
@@ -34,22 +35,34 @@ object AllInstrument {
   }
 
   /**
-   * Example: a8e458e619d5235fe8a97ea0186961acc1192ed13bb28a79b493c752747cc683
    * @param m
    */
-  private def instrumentStartTimer(m: SootMethod): Unit = {
+  private def instrumentMethod(m: SootMethod): Unit = {
     val swaps = mutable.Map[Stmt, Stmt]()
     for (unit <- m.getActiveBody.getUnits.asScala) {
       val stmt = unit.asInstanceOf[Stmt]
       if (stmt.containsInvokeExpr()) {
         val invokeExpr = stmt.getInvokeExpr
         if (invokeExpr.getMethod.getSignature == "<android.os.CountDownTimer: android.os.CountDownTimer start()>") {
+          // Example: a8e458e619d5235fe8a97ea0186961acc1192ed13bb28a79b493c752747cc683
           val timer = invokeExpr.asInstanceOf[InstanceInvokeExpr].getBase.asInstanceOf[Local]
           val timerClass = timer.getType.asInstanceOf[RefType].getSootClass
           val onFinish = timerClass.getMethodByNameUnsafe("onFinish")
           val invocation = Jimple.v().newInvokeStmt(Jimple.v().newVirtualInvokeExpr(timer, onFinish.makeRef()))
           CallGraphManager.updateCall(m, stmt, invocation, onFinish)
           swaps.put(stmt, invocation)
+        } else if (AppInfo.hier.isSubclassOf(invokeExpr.getMethod.getDeclaringClass, Constants.dialogFragmentClass)) {
+          if (invokeExpr.getMethod.getSubSignature == "void <init>()") {
+            val instanceInvokeExpr = invokeExpr.asInstanceOf[InstanceInvokeExpr]
+            val baseLocal = instanceInvokeExpr.getBase
+            val baseClass = baseLocal.getType.asInstanceOf[RefType].getSootClass
+            DynamicCFG.getRunnerofDialogFragment(baseClass) match {
+              case Some(runner) =>
+                val invocation = Jimple.v().newInvokeStmt(Jimple.v().newVirtualInvokeExpr(baseLocal.asInstanceOf[Local], runner.makeRef()))
+                CallGraphManager.updateCall(m, stmt, invocation, runner.method)
+                swaps.put(stmt, invocation)
+            }
+          }
         }
       }
     }
@@ -63,7 +76,7 @@ object AllInstrument {
       for (method <- activity.getMethods.asScala) {
         if (method.isConcrete && method.hasActiveBody) {
           // FIXME: remove the mainActivity workaround by fixing this
-          // instrumentStartTimer(method)
+          instrumentMethod(method)
         }
       }
     }
